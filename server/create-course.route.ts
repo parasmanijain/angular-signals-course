a785041020f44e1a55de175a287597068ec3468c
Parasmani Jain <parasmani.jain2208@gmail.com>
2025-12-19 19:54:15 +0530

    code updated

--- server/create-course.route.ts
+++ server/create-course.route.ts
@@ -1,18 +1,17 @@
-import {Request, Response} from 'express';
-import {COURSES} from './db-data';
+import { Request, Response } from 'express';
+import { COURSES } from './db-data';
 
 export var coursesKeyCounter = 100;
 
 export function createCourse(req: Request, res: Response) {
-
-  console.log("Creating new course ...");
+  console.log('Creating new course ...');
 
   const changes = req.body;
 
   const newCourse = {
     id: coursesKeyCounter,
     seqNo: coursesKeyCounter,
-    ...changes
+    ...changes,
   };
 
   COURSES[newCourse.id] = newCourse;
@@ -20,9 +19,6 @@ export function createCourse(req: Request, res: Response) {
   coursesKeyCounter += 1;
 
   setTimeout(() => {
-
     res.status(200).json(newCourse);
-
   }, 1500);
-
 }
--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,5 @@
-import * as express from 'express';
-import { Application } from 'express';
-import * as cors from 'cors';
+import express, { Application } from 'express';
+import cors from 'cors';
 import { getAllCourses, getCourseById } from './get-courses.route';
 import { searchLessons } from './search-lessons.route';
 import { saveCourse } from './save-course.route';
@@ -34,6 +33,6 @@ app.route('/api/login').post(loginUser);
 const httpServer = app.listen(9000, () => {
   console.log(
     'HTTP REST API Server running at http://localhost:' +
-      httpServer.address()['port']
+      httpServer.address()['port'],
   );
 });
